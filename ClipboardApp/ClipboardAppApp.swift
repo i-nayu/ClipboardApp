bb71f588526e6ee0cf3a42217c5b2ebf0eebd9b3
nayu <is0762ix@ed.ritsumei.ac.jp>
2026-03-19 01:14:49 +0900

    Initial Commit

---
+++ ClipboardApp/ClipboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardAppApp.swift
+//  ClipboardApp
+//
+//  Created by Nayu Igami on 2026/03/19.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
